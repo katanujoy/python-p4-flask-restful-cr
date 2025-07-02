8af9afc742e99140d10bdd6c26011e1cbc6695b0
katanujoy <katanujoyy99@gmail.com>
2025-07-02 07:46:09 +0300

    completed flas restful cr

--- server/models.py
+++ server/models.py
@@ -1,16 +1,65 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+#!/usr/bin/env python3
 
-db = SQLAlchemy()
+from flask import Flask, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-class Newsletter(db.Model, SerializerMixin):
-    __tablename__ = 'newsletters'
+from models import db, Newsletter
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    body = db.Column(db.String)
-    published_at = db.Column(db.DateTime, server_default=db.func.now())
-    edited_at = db.Column(db.DateTime, onupdate=db.func.now())
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-    def __repr__(self):
-        return f'<Newsletter {self.title}, published at {self.published_at}.>'
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+# Home Route
+class Home(Resource):
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(response_dict, 200)
+        return response
+
+# All Newsletters Route (GET and POST)
+class Newsletters(Resource):
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+
+    def post(self):
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+        response = make_response(response_dict, 201)
+        return response
+
+# Single Newsletter by ID Route
+class NewsletterByID(Resource):
+    def get(self, id):
+        record = Newsletter.query.filter_by(id=id).first()
+        if record:
+            response = make_response(record.to_dict(), 200)
+        else:
+            response = make_response({"error": "Newsletter not found"}, 404)
+        return response
+
+# Register Resources
+api.add_resource(Home, '/')
+api.add_resource(Newsletters, '/newsletters')
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
